174521362d255f608a497cd4d152efa863780859
Igor Varshavbsky <igor.varshavsky@tristar.com>
2023-12-31 16:53:05 -0500

    First commit

---
+++ src/main/java/ca/ivar/MyListOfStuffToDo/MyListOfStuffToDoApplication.java
@@ -0,0 +1,13 @@
+package ca.ivar.MyListOfStuffToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyListOfStuffToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyListOfStuffToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/ivar/MyListOfStuffToDo/MyListOfStuffToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ivar.MyListOfStuffToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyListOfStuffToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
